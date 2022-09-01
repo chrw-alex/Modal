97d751de7415a8801e1fcd12f0ffded3ba00ec96
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-01 15:35:15 +0300

    first commit

---
+++ modal.js
@@ -0,0 +1,46 @@
+function closeModal(modalSelector) {
+  const modal = document.querySelector(modalSelector);
+
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+  document.body.style.overflow = '';
+}
+
+function openModal(modalSelector) {
+  const modal = document.querySelector(modalSelector);
+
+  modal.classList.add('show');
+  modal.classList.remove('hide');
+  document.body.style.overflow = 'hidden';
+}
+
+function modal(triggerSelector, modalSelector) {
+  const modalTrigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector);
+
+  modalTrigger.forEach(btn => {
+      btn.addEventListener('click', () => openModal(modalSelector));
+  });
+
+  modal.addEventListener('click', (e) => {
+      if (e.target === modal || e.target.getAttribute('data-close') == "") {
+          closeModal(modalSelector);
+      }
+  });
+
+  document.addEventListener('keydown', (e) => {
+      if (e.code === "Escape" && modal.classList.contains('show')) { 
+          closeModal(modalSelector);
+      }
+  });
+
+  function showModalByScroll() {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+          openModal(modalSelector, modalTimerId);
+          window.removeEventListener('scroll', showModalByScroll);
+      }
+  }
+  window.addEventListener('scroll', showModalByScroll);
+}
+
+modal('[data-modal]', '.modal');
